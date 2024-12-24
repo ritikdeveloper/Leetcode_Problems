4a68b52fdd415f07333f15f8391e0cef072a2ac6
ritik mehrotra <ritik1.mehrotra@gmail.com>
2024-12-24 19:12:40 +0530

    Time: 2 ms (29.66%), Space: 57.9 MB (5.66%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.java
@@ -0,0 +1,43 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        if(nums.length==0||nums.length==1){
+            return ;
+        }
+        while(k>nums.length){
+            k=k-nums.length;
+        }
+        //so first we will revse the array
+
+        int start =0;
+        int end = nums.length-1;
+        while(start<end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end]=temp;
+            start++;
+            end--;
+        }
+
+        //now we will normalize first k element;
+        start=0;
+        end=k-1;
+        while(start<end){
+           int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end]=temp; 
+            start++;
+            end--;
+        }
+        //now we will reverse other elements;
+        start = k;
+        end=nums.length-1;
+        while(start<end){
+           int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end]=temp;
+            start++;
+            end--; 
+        }
+
+    }
+}
