fd63ebb0afaab978d0e99a736ccb8e410d1fb017
ritik mehrotra <ritik1.mehrotra@gmail.com>
2024-12-30 17:45:17 +0530

    Time: 13 ms (72.59%), Space: 44.7 MB (61.56%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,35 +1,40 @@
 class Solution {
-    public int maxVowels(String s, int k) {
-        int left=0;
-        int right = k-1;
-        int vowelCount=0;
-        for(int i=0;i<=right;i++){
-            if(s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'||s.charAt(i)=='u') vowelCount++;
 
+    public int maxVowels(String s, int k) {
+        int left = 0;
+        int right = k - 1;
+        int currentVowelCount = 0;
+
+        
+
+        for (int i = 0; i <= right; i++) {
+            if (isVowel(s.charAt(i))) {
+                currentVowelCount++;
+            }
         }
 
-
-        int totalMax=vowelCount;
+        int maxVowels = currentVowelCount;
         left++;
         right++;
-        while(right<s.length()){
-            if(s.charAt(left -1)=='a'||
-            s.charAt(left -1)=='e'||
-            s.charAt(left -1)=='i'||
-            s.charAt(left -1)=='o'||
-            s.charAt(left -1)=='u')
-            vowelCount--;
-            if(s.charAt(right)=='a'||
-            s.charAt(right)=='e'||
-            s.charAt(right)=='i'||
-            s.charAt(right)=='o'||
-            s.charAt(right)=='u') 
-            vowelCount++;
-            totalMax=Math.max(totalMax,vowelCount);
-            right++;
-            left++;
 
+        while (right < s.length()) {
+            if (isVowel(s.charAt(left - 1))) {
+                currentVowelCount--;
+            }
+
+            if (isVowel(s.charAt(right))) {
+                currentVowelCount++;
+            }
+
+            maxVowels = Math.max(maxVowels, currentVowelCount);
+
+            left++;
+            right++;
         }
-        return totalMax;
+
+        return maxVowels;
     }
+    boolean isVowel(char c) {
+            return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+        }
 }
