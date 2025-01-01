2d2d5c2caa1f78b7f062703a69f5dc356bdb8679
ritik mehrotra <ritik1.mehrotra@gmail.com>
2025-01-01 13:30:39 +0530

    Time: 1 ms (96.76%), Space: 45.9 MB (26.51%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -26,8 +26,8 @@ class Solution {
             nums[len - 1] = 1;
             n--;
         }
+        if(n==0)return true;
 
-        // Check the middle plots.
         for (int i = 1; i < len - 1; i++) {
             if (nums[i] == 0 && nums[i - 1] == 0 && nums[i + 1] == 0) {
                 nums[i] = 1;
