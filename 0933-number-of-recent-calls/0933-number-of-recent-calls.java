eb262c1a9497229ec11bd914215ca92225c72e0f
ritik mehrotra <ritik1.mehrotra@gmail.com>
2025-01-04 22:55:03 +0530

    Time: 19 ms (88.83%), Space: 53.3 MB (80.57%) - LeetHub

---
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.java
@@ -0,0 +1,27 @@
+class RecentCounter {
+    int[] records = new int[10000]; //
+
+    int start =0;
+    int end =0;
+    
+    public RecentCounter() {
+        start =0;
+        end =0;
+
+    }
+    
+    public int ping(int t) {
+        while(start<end&&(t-records[start]>3000)){
+            start++;
+        }
+        records[end++] = t;
+        return end -start;
+    }
+}
+
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */
